c7c046e08fce8cc337e8bc94eee7425176b37f97
Agga <agga@Aggas-MacBook-Air.local>
2024-06-29 16:12:20 +0700

    [add] env project

--- database/env.py
+++ database/env.py
@@ -1,6 +1,7 @@
 # ENV = {}
 from db import Base
 
+
 class Env(object):
     models: dict[str, object] = {}
 
@@ -12,14 +13,11 @@ class Env(object):
 
     def start(self):
         # Instantiate all model classes stored in the dictionary
-        # for name, cls in self.models.items():
-        #     # Create a new class that inherits from the original class
-        #     new_class = type(cls.__name__, (cls,Base), cls.__dict__.copy())
-        #     # Replace the original class with the new class in the dictionary
-        #     self.models[name] = new_class()
-
-        self.models = {name: cls() for name, cls in self.models.items()}
+        for name, cls in self.models.items():
+            # print(name, cls)
+            cls.__bases__ = (Base,) + tuple(cls.__bases__)
+            self.models[name] = cls()
+            # print(cls.__bases__)
 
 
-#
 ENV = Env()
